d48a1d818e659bc43da63f7f054fb411091887fa
Boris Diadus <boris@diadus.net>
2019-01-24 15:33:19 +0200

    Interface of the Settings page

--- admin/partials/meetup-bridge-settings-display.php
+++ admin/partials/meetup-bridge-settings-display.php
@@ -19,8 +19,21 @@ $meetup_group_slug = get_option('meetup-bridge-meetup-group-slug');
 <div class="wrap">
     <h1 class="wp-heading-inline">Meetup-Bridge Settings</h1>
     <form method="post">
-        <input type="text" name="meetup-api" value="<?php echo $meetup_api; ?>"/>
-        <input type="text" name="meetup-group-slug" value="<?php echo $meetup_group_slug; ?>"/>
+        <table class="form-table">
+
+            <tbody><tr>
+                <th scope="row"><label for="meetup-api">Meetup API Key</label></th>
+                <td><input class="regular-text" type="text" name="meetup-api" value="<?php echo $meetup_api; ?>"/>
+                    <p class="description" id="tagline-description">You can get it there -> <a href="https://secure.meetup.com/meetup_api/key/">https://secure.meetup.com/meetup_api/key/</a></p></td>
+                </td>
+            </tr>
+
+            <tr>
+                <th scope="row"><label for="meetup-group-slug">Meetup Group URL (slug)</label></th>
+                <td><input class="regular-text" type="text" name="meetup-group-slug" value="<?php echo $meetup_group_slug; ?>"/>
+                    <p class="description" id="tagline-description">For example, highlighted part of <span style="color: hsla(0, 0%, 76%, 1);">https://www.meetup.com/<span style="color: #000;">Web-Summit-SaaS-Mojitos</span>/events/</span></p></td>
+            </tr>
+            </tbody></table>
         <br/><br/>
         <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
 
